82b423edcbec6183b978311f6f5104eb3da6c8dc
Helen Shatynska <helen.shatynska@gmail.com>
2023-05-04 22:33:12 +0300

    git init

---
+++ public/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                учасники
+            </tr>
+            <tr>
+                травень
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    Коцюба Галина 
+                </td>
+                <td>
+                    500
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Дробчак Ольга
+                </td>
+                <td>
+                    200
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Некрасова Валентина
+                </td>
+                <td>
+                    200
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Созанська Ірина
+                </td>
+                <td>
+                    200
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Шатинська Альона
+                </td>
+                <td>
+                    200
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Шатинська Альона
+                </td>
+                <td>
+                    1300
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
