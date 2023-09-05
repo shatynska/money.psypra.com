17511bb440c95fa9d2e89846c6117f2eb95b1463
Helen Shatynska <helen.shatynska@gmail.com>
2023-09-05 18:48:15 +0300

    Add RouterProvider to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-function App() {
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routers/router';
 
-  return (
-    <>
-    </>
-  )
+function App() {
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
