5cbfd011ebada533d7c4c1d4462302e700b5bbf1
Helen Shatynska <helen.shatynska@gmail.com>
2023-09-06 17:44:15 +0300

    Add ToastContainer to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.scss'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.scss';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <ToastContainer position="bottom-left" autoClose={2000} />
   </React.StrictMode>,
-)
+);
