4e98ca3db6a73e1c689ef255c94f863bf2672ee8
Helen Shatynska <helen.shatynska@gmail.com>
2023-09-06 09:14:50 +0300

    Add form for login and registration in Auth

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,9 +1,37 @@
-import { FC } from 'react'
+import { FC, useState } from 'react';
 
 const Auth: FC = () => {
+  const [isLogin, setIsLogin] = useState<boolean>(true);
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
   return (
-    <div>Auth</div>
-  )
-}
+    <div>
+      <h1>{isLogin ? 'Вхід' : 'Реєстрація'}</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">{isLogin ? 'Увійти' : 'Зареєструватися'}</button>
+      </form>
 
-export default Auth
+      {isLogin ? (
+        <button onClick={() => setIsLogin(!isLogin)}>
+          Не маєте акаунту? Зареєструватися
+        </button>
+      ) : (
+        <button onClick={() => setIsLogin(!isLogin)}>
+          Маєте акаунт? Увійти
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Auth;
