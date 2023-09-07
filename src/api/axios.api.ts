a97aef08bcbc0d3c0b60074aa542730739b4e533
Helen Shatynska <helen.shatynska@gmail.com>
2023-09-07 19:57:05 +0300

    Move ulr of api to .env file

--- src/api/axios.api.ts
+++ src/api/axios.api.ts
@@ -2,8 +2,9 @@ import axios from 'axios';
 import { getTokenFromLocalStorage } from '../helpers/localstorage.helper';
 
 export const instance = axios.create({
-  baseURL: 'http://dro.psypra.com:3001/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     Authorization: 'Bearer ' + getTokenFromLocalStorage() || '',
   },
 });
+
