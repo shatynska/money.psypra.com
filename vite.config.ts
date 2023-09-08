166c166b5c1ae478a42b9ddf12bc4a4e8dc106af
Helen Shatynska <helen.shatynska@gmail.com>
2023-09-08 10:23:12 +0300

    Set host , strictPort and port in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
     emptyOutDir: true,
   },
   server: {
+    host: true,
+    strictPort: true,
+    port: 5173,
     proxy: {
       '/api': {
         target: process.env.VITE_API_BASE_URL,
