ba28121a2be6bb3c40ad08618d632363691293d0
Helen Shatynska <helen.shatynska@gmail.com>
2023-09-11 14:14:07 +0300

    Create queryClient and provide it to the app

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,15 @@ import App from './App.tsx';
 import './index.scss';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-    <ToastContainer position="bottom-left" autoClose={2000} />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ToastContainer position="bottom-left" autoClose={2000} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
