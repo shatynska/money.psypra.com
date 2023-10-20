822a7d7ee6cec976bd101b47aefdf7daa2817dff
Helen Shatynska <helen.shatynska@gmail.com>
2023-10-20 17:25:20 +0300

    Add alias @

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
+  },
   build: {
     outDir: 'build',
     assetsDir: 'assets',
