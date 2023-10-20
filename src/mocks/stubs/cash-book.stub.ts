b609751d20518d5e910fdc40c85ff9e9c6b129ca
Helen Shatynska <helen.shatynska@gmail.com>
2023-10-20 23:54:10 +0300

    Add CASH_BOOK_STUB in mocks folder

---
+++ src/mocks/stubs/cash-book.stub.ts
@@ -0,0 +1,153 @@
+import { CashBook } from '@/types';
+
+export const CASH_BOOK_STUB: CashBook = {
+  id: '1',
+  cashBookYear: '2023',
+  cashBalance: '5410',
+  members: [
+    {
+      id: '1',
+      name: 'Берчук Володимир',
+      membershipFees: [
+        { id: '', month: '10', amount: '200' },
+        { id: '', month: '09', amount: '200' },
+        { id: '', month: '07', amount: '200' },
+        { id: '', month: '06', amount: '200' },
+        { id: '', month: '05', amount: '' },
+      ],
+    },
+    {
+      id: '2',
+      name: 'Білас Юлія',
+      membershipFees: [
+        { id: '', month: '10', amount: '200' },
+        { id: '', month: '09', amount: '200' },
+        { id: '', month: '07', amount: '200' },
+        { id: '', month: '06', amount: '' },
+        { id: '', month: '05', amount: '' },
+      ],
+    },
+    {
+      id: '3',
+      name: 'Гамбарова Еляна',
+      membershipFees: [
+        { id: '', month: '10', amount: '200' },
+        { id: '', month: '09', amount: '200' },
+        { id: '', month: '07', amount: '200' },
+        { id: '', month: '06', amount: '' },
+        { id: '', month: '05', amount: '' },
+      ],
+    },
+    {
+      id: '4',
+      name: 'Дробчак Ольга',
+      membershipFees: [
+        { id: '', month: '10', amount: '200' },
+        { id: '', month: '09', amount: '200' },
+        { id: '', month: '07', amount: '200' },
+        { id: '', month: '06', amount: '200' },
+        { id: '', month: '05', amount: '200' },
+      ],
+    },
+    {
+      id: '5',
+      name: 'Коцюба Галина',
+      membershipFees: [
+        { id: '', month: '10', amount: '' },
+        { id: '', month: '09', amount: '' },
+        { id: '', month: '07', amount: '' },
+        { id: '', month: '06', amount: '200' },
+        { id: '', month: '05', amount: '500' },
+      ],
+    },
+    {
+      id: '6',
+      name: 'Лех Наталія',
+      membershipFees: [
+        { id: '', month: '10', amount: '200' },
+        { id: '', month: '09', amount: '200' },
+        { id: '', month: '07', amount: '200' },
+        { id: '', month: '06', amount: '200' },
+        { id: '', month: '05', amount: '200' },
+      ],
+    },
+    {
+      id: '7',
+      name: 'Некрасова Валентина',
+      membershipFees: [
+        { id: '', month: '10', amount: '200' },
+        { id: '', month: '09', amount: '200' },
+        { id: '', month: '07', amount: '200' },
+        { id: '', month: '06', amount: '200' },
+        { id: '', month: '05', amount: '200' },
+      ],
+    },
+    {
+      id: '8',
+      name: 'Петришин Ольга',
+      membershipFees: [
+        { id: '', month: '10', amount: '' },
+        { id: '', month: '09', amount: '200' },
+        { id: '', month: '07', amount: '200' },
+        { id: '', month: '06', amount: '200' },
+        { id: '', month: '05', amount: '200' },
+      ],
+    },
+    {
+      id: '9',
+      name: 'Созанська Ірина',
+      membershipFees: [
+        { id: '', month: '10', amount: '200' },
+        { id: '', month: '09', amount: '200' },
+        { id: '', month: '07', amount: '200' },
+        { id: '', month: '06', amount: '200' },
+        { id: '', month: '05', amount: '200' },
+      ],
+    },
+    {
+      id: '10',
+      name: 'Татаревич Галина',
+      membershipFees: [
+        { id: '', month: '10', amount: '200' },
+        { id: '', month: '09', amount: '' },
+        { id: '', month: '07', amount: '' },
+        { id: '', month: '06', amount: '' },
+        { id: '', month: '05', amount: '' },
+      ],
+    },
+    {
+      id: '11',
+      name: 'Франчук Тетяна',
+      membershipFees: [
+        { id: '', month: '10', amount: '' },
+        { id: '', month: '09', amount: '' },
+        { id: '', month: '07', amount: '200' },
+        { id: '', month: '06', amount: '' },
+        { id: '', month: '05', amount: '' },
+      ],
+    },
+    {
+      id: '12',
+      name: 'Чапля Уляна',
+      membershipFees: [
+        { id: '', month: '10', amount: '200' },
+        { id: '', month: '09', amount: '200' },
+        { id: '', month: '07', amount: '200' },
+        { id: '', month: '06', amount: '200' },
+        { id: '', month: '05', amount: '200' },
+      ],
+    },
+    {
+      id: '13',
+      name: 'Шатинська Альона',
+      membershipFees: [
+        { id: '', month: '10', amount: '200' },
+        { id: '', month: '09', amount: '200' },
+        { id: '', month: '07', amount: '200' },
+        { id: '', month: '06', amount: '200' },
+        { id: '', month: '05', amount: '200' },
+      ],
+    },
+  ],
+  expenses: [],
+};
