14e1e8920e04f2f82b45c8d4532b7a12f77c3928
Helen Shatynska <helen.shatynska@gmail.com>
2023-10-25 22:18:05 +0300

    Put logic of getting cash books in Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,29 @@
-import { FC } from 'react';
+import { useQuery } from '@tanstack/react-query';
 
-import { CashBook } from '@/components';
+import { CashBooksList, CurrentCashBook } from '@/components';
+import { CashBooksService } from '@/services';
 
-export const Home: FC = () => {
-  return <CashBook />;
+export const Home = () => {
+  const {
+    data: response,
+    isLoading,
+    error,
+  } = useQuery(['get cash books'], () => CashBooksService.getCashBooks());
+  const cashBooks = response?.data;
+
+  return (
+    <>
+      {error instanceof Error && <div>{error.message}</div>}
+      {isLoading ? (
+        <span>Завантаження ...</span>
+      ) : cashBooks ? (
+        <>
+          <CashBooksList list={cashBooks.cashBooksList} />
+          <CurrentCashBook cashBook={cashBooks.currentCashBook} />
+        </>
+      ) : (
+        <span> Касова книга не знайдена</span>
+      )}
+    </>
+  );
 };
