c39a24aa184ec9e38857e44cdf37298921b7a374
Helen Shatynska <helen.shatynska@gmail.com>
2023-10-26 10:26:28 +0300

    Destructure cashBook proper in CurrentCashBook

--- src/components/CurrentCashBook.tsx
+++ src/components/CurrentCashBook.tsx
@@ -5,26 +5,28 @@ export const CurrentCashBook = ({
 }: {
   cashBook: CashBookBriefly;
 }) => {
+  const { cashBalance, membersWithMembershipFees, reportingMonths } = cashBook;
+
   return (
     <div>
-      <h1>Баланс: {cashBook.cashBalance} грн </h1>
+      <h1>Баланс: {cashBalance} грн </h1>
       <h2>Внески</h2>
       <table>
         <thead>
           <tr>
             <td>учасники</td>
-            {cashBook.reportingMonths.map((month) => {
+            {reportingMonths.map((month) => {
               return <td key={month}>{month}</td>;
             })}
           </tr>
         </thead>
         <tbody>
-          {cashBook.membersWithMembershipFees.map((member) => {
+          {membersWithMembershipFees.map((member) => {
             const fees = new Map(member.membershipFees);
             return (
               <tr>
                 <td>{member.name}</td>
-                {cashBook.reportingMonths.map((month) => {
+                {reportingMonths.map((month) => {
                   const paidFee = fees.get(month);
                   return <td key={month}>{paidFee ?? ''}</td>;
                 })}
