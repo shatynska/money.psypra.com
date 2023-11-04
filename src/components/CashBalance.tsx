6042a5b5148d06666cdad5c558faf29eeb9bdddc
Helen Shatynska <helen.shatynska@gmail.com>
2023-11-05 00:40:25 +0200

    Move query logic to CurrentCashBook & CashBalance

--- src/components/CashBalance.tsx
+++ src/components/CashBalance.tsx
@@ -1,3 +1,27 @@
-export const CashBalance = ({ value }: { value: number }) => {
-  return <h1>Баланс: {value} грн </h1>;
+import { CashBooksService } from '@/services';
+import { useQuery } from '@tanstack/react-query';
+
+export const CashBalance = () => {
+  const {
+    data: response,
+    isLoading,
+    error,
+  } = useQuery(['get cash balance'], () => CashBooksService.getCashBalance());
+  const cashBalance = response?.data;
+
+  return (
+    <>
+      {error instanceof Error && <div>{error.message}</div>}
+      {isLoading ? (
+        <span>Завантаження ...</span>
+      ) : cashBalance ? (
+        <section className="absolute right-0 top-0 flex h-32 w-32 flex-col items-center justify-center bg-foreground font-semibold text-background">
+          <h2 className="opacity-70">баланс</h2>
+          <div className="text-[32px] ">{cashBalance.value}</div>
+        </section>
+      ) : (
+        <span>баланс не знайдено</span>
+      )}
+    </>
+  );
 };
--- src/components/CurrentCashBook.tsx
+++ src/components/CurrentCashBook.tsx
@@ -1,20 +1,34 @@
-import { CashBalance, MembershipFees } from '@/components';
-import { CashBookBriefly } from '@/types';
+import { CashBooksService } from '@/services';
+import { useQuery } from '@tanstack/react-query';
+import { MembershipFees } from '.';
 
-export const CurrentCashBook = ({
-  cashBook,
-}: {
-  cashBook: CashBookBriefly;
-}) => {
-  const { cashBalance, membersWithMembershipFees, reportingMonths } = cashBook;
+export const CurrentCashBook = () => {
+  const {
+    data: response,
+    isLoading,
+    error,
+  } = useQuery(['get current cash books'], () =>
+    CashBooksService.getCurrentCashBook(),
+  );
+  const currentCashBook = response?.data;
 
   return (
     <>
-      <CashBalance value={cashBalance} />
-      <MembershipFees
-        reportingMonths={reportingMonths}
-        membersWithMembershipFees={membersWithMembershipFees}
-      />
+      {error instanceof Error && <div>{error.message}</div>}
+      {isLoading ? (
+        <span>Завантаження ...</span>
+      ) : currentCashBook ? (
+        <>
+          <MembershipFees
+            reportingMonths={currentCashBook.reportingMonths}
+            membersWithMembershipFees={
+              currentCashBook.membersWithMembershipFees
+            }
+          />{' '}
+        </>
+      ) : (
+        <span> Касова книга не знайдена</span>
+      )}
     </>
   );
 };
