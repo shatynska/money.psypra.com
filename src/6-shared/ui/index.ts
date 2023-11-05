34e47a2581a704b0d163d3e712f17037e426960f
Helen Shatynska <helen.shatynska@gmail.com>
2023-11-05 20:32:00 +0200

    Add logotype.tsx to shared/ui

--- src/6-shared/ui/index.ts
+++ src/6-shared/ui/index.ts
@@ -1,1 +1,2 @@
+export * from './logotype';
 export * from './table';
---
+++ src/6-shared/ui/logotype.tsx
@@ -0,0 +1,8 @@
+export const Logotype = () => {
+  return (
+    <span className="text-background">
+      Cash
+      <span className="text-foreground">Book</span>
+    </span>
+  );
+};
