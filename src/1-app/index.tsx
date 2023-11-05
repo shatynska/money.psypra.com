e5931d1e39c5739327227266bcb857a30f0fae9f
Helen Shatynska <helen.shatynska@gmail.com>
2023-11-05 18:46:39 +0200

    Combine App.tsx and main.tsx into index.tsx

--- src/1-app/index.tsx
+++ src/1-app/index.tsx
@@ -1,9 +1,11 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import App from './App';
+import { router } from './router';
+
 import './index.css';
 
 if (process.env.NODE_ENV === 'development') {
@@ -16,7 +18,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
       <ToastContainer position="bottom-left" autoClose={2000} />
     </QueryClientProvider>
   </React.StrictMode>,
