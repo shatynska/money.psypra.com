0f47ff93927967ef4c3527fe4b862962b73f07c7
Helen Shatynska <helen.shatynska@gmail.com>
2023-11-09 23:31:58 +0200

    Add extra condition to start worker & change path

--- src/1-app/index.tsx
+++ src/1-app/index.tsx
@@ -1,25 +1,27 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import './index.css';
 import { router } from './router';
 
-import './index.css';
-
-if (process.env.NODE_ENV === 'development') {
-  const { worker } = await import('@/mocks/browser');
+if (
+  import.meta.env.VITE_API_WITH_MSW &&
+  process.env.NODE_ENV === 'development'
+) {
+  const { worker } = await import('~/shared/lib/msw/browser');
   worker.start();
 }
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <ToastContainer position="bottom-left" autoClose={2000} />
     </QueryClientProvider>
-  </React.StrictMode>,
+  </StrictMode>,
 );
