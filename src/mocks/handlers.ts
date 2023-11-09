dfaa0f9780466f134ede7dc2697970226b124654
Helen Shatynska <helen.shatynska@gmail.com>
2023-11-09 23:22:52 +0200

    Remove handlers in mocks & move to shared/lib/msw

--- src/mocks/handlers.ts
+++
@@ -1,9 +0,0 @@
-import { CashBalanceHandlers } from '~/entities/cash-balance/api/mocks';
-import { CurrentCashBookHandlers } from '~/entities/current-cash-book/api/mocks';
-import { UsersHandlers } from '~/entities/users/api/mocks';
-
-export const handlers = [
-  ...CashBalanceHandlers,
-  ...CurrentCashBookHandlers,
-  ...UsersHandlers,
-];
