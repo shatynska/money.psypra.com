387c9f4fc883484b75bc7483987f635248a9adaa
Helen Shatynska <helen.shatynska@gmail.com>
2023-11-11 22:45:58 +0200

    Refactor checkMSWNecessary() in shared layer

--- src/6-shared/lib/msw/check-msw-necessary.ts
+++ src/6-shared/lib/msw/check-msw-necessary.ts
@@ -1,9 +1,10 @@
-export const checkMSWNecessary = async () => {
+import { worker } from './browser';
+
+export const checkMSWNecessary = () => {
   if (
     import.meta.env.VITE_API_WITH_MSW &&
     process.env.NODE_ENV === 'development'
   ) {
-    const { worker } = await import('./browser');
     worker.start();
   }
 };
--- src/6-shared/lib/msw/index.ts
+++ src/6-shared/lib/msw/index.ts
@@ -1,2 +1,3 @@
 export { worker } from './browser';
+export { checkMSWNecessary } from './check-msw-necessary';
 export { server } from './server';
