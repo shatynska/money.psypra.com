6505dbe9b81bf59fb78ded6fb14e42845baf613f
Helen Shatynska <helen.shatynska@gmail.com>
2023-11-15 18:28:11 +0200

    Add types to current-cash-book slice for a while

--- src/5-entities/current-cash-book/index.ts
+++ src/5-entities/current-cash-book/index.ts
@@ -1,1 +1,2 @@
+export type * from './types';
 export { CurrentCashBook, MembershipFees } from './ui';
---
+++ src/5-entities/current-cash-book/types.ts
@@ -0,0 +1,32 @@
+export interface CurrentCashBook {
+  year: number;
+  reportingMonths: number[];
+  membersWithMembershipFees: MemberWithMembershipFees[];
+  expenses: Expense[];
+}
+
+export interface Expense {
+  id: string;
+  date: string;
+  amount: number;
+  details: string;
+}
+
+export interface Member {
+  id: string;
+  name: string;
+  status: string;
+}
+
+export interface MembershipFee {
+  id: string;
+  date: string;
+  reportingMonth: number;
+  memberName: string;
+  amount: number;
+}
+
+export interface MemberWithMembershipFees {
+  name: string;
+  membershipFees: Array<[number, number]>;
+}
