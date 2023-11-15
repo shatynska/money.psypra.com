d046a9524db48459827df70102cffb3ec8c30d5f
Helen Shatynska <helen.shatynska@gmail.com>
2023-11-15 18:14:03 +0200

    Remove api folder in current-cash-book slice

--- src/5-entities/current-cash-book/api/mocks/handlers.ts
+++
@@ -1,9 +0,0 @@
-import { rest } from 'msw';
-import { getAbsoluteApiUrl } from '~/shared/lib';
-import { CURRENT_CASH_BOOK_STUB } from './stubs';
-
-export const CurrentCashBookHandlers = [
-  rest.get(getAbsoluteApiUrl('/cash-books/current'), (_req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(CURRENT_CASH_BOOK_STUB));
-  }),
-];
