23e16c3e97cffbc71dcdd3e232053980219b2909
Helen Shatynska <helen.shatynska@gmail.com>
2023-11-17 13:47:16 +0200

    Init storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,7 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    '@feature-sliced',
-    '@feature-sliced/eslint-config/rules/import-order/experimental',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', '@feature-sliced', '@feature-sliced/eslint-config/rules/import-order/experimental', 'plugin:storybook/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'src/shared/api/generated'],
   parser: '@typescript-eslint/parser',
   settings: {
