36ba85a9291c965472595a651927fc790aa73218
Helen Shatynska <helen.shatynska@gmail.com>
2023-11-24 15:28:33 +0200

    Remove mws and faker plugins from kubb.config

--- kubb.config.js
+++ kubb.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from '@kubb/core';
 import createSwagger from '@kubb/swagger';
-import createSwaggerFaker from '@kubb/swagger-faker';
-import createSwaggerMsw from '@kubb/swagger-msw';
 import createSwaggerTanstackQuery from '@kubb/swagger-tanstack-query';
 import createSwaggerTS from '@kubb/swagger-ts';
 import createSwaggerZod from '@kubb/swagger-zod';
@@ -13,7 +11,6 @@ export default defineConfig({
   },
   output: {
     path: './src/shared/api/generated',
-    clean: true,
   },
   hooks: {
     done: ['prettier --write "**/*.{ts,tsx}"'],
@@ -30,45 +27,12 @@ export default defineConfig({
     }),
     createSwaggerTanstackQuery({
       output: './hooks',
-      skipBy: [
-        {
-          type: 'tag',
-          pattern: 'store',
-        },
-      ],
       groupBy: { type: 'tag' },
       client: './src/shared/api/client.ts',
       framework: 'react',
     }),
     createSwaggerZod({
       output: './zod',
-      skipBy: [
-        {
-          type: 'tag',
-          pattern: 'store',
-        },
-      ],
-      groupBy: { type: 'tag' },
-    }),
-    createSwaggerFaker({
-      output: 'mocks',
-      skipBy: [
-        {
-          type: 'tag',
-          pattern: 'store',
-        },
-      ],
-      groupBy: { type: 'tag' },
-      dateType: 'date',
-    }),
-    createSwaggerMsw({
-      output: 'msw',
-      skipBy: [
-        {
-          type: 'tag',
-          pattern: 'store',
-        },
-      ],
       groupBy: { type: 'tag' },
     }),
   ],
