0a9aadd59cada031fb253768c084acf4d2622aaa
Helen Shatynska <helen.shatynska@gmail.com>
2023-11-30 09:10:12 +0200

    Add ErrorBoundary to CashBalance component

--- src/entities/cash-balance/ui/CashBalance.tsx
+++ src/entities/cash-balance/ui/CashBalance.tsx
@@ -1,4 +1,5 @@
 import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 
 // eslint-disable-next-line import/no-internal-modules
 import { useCashBooksControllerGetCashBalance } from '~/shared/api/generated/hooks/cashBooksController';
@@ -8,11 +9,14 @@ export function CashBalance() {
   const { data: cashBalance } = useCashBooksControllerGetCashBalance();
 
   return (
-    <Suspense fallback={<Skeleton />}>
-      <section className="absolute -top-4 right-0 flex h-32 w-32 flex-col items-center justify-center bg-foreground font-semibold text-background">
-        <h2 className="opacity-70">баланс</h2>
-        <div className="text-[32px] ">{cashBalance?.value}</div>
-      </section>
-    </Suspense>
+    <section className="absolute -top-4 right-0 flex h-32 w-32 flex-col items-center justify-center bg-foreground font-semibold text-background">
+      <h2 className="opacity-70">баланс</h2>
+      //TODO Add error boundary fallback
+      <ErrorBoundary fallback={null}>
+        <Suspense fallback={<Skeleton />}>
+          <div className="text-[32px] ">{cashBalance?.value}</div>
+        </Suspense>
+      </ErrorBoundary>
+    </section>
   );
 }
