762385950e83d30e3053e4afff22af2e7f1e3fcd
Helen Shatynska <helen.shatynska@gmail.com>
2023-12-01 22:21:22 +0200

    Separate MemberTableRow component

---
+++ src/entities/current-cash-book/ui/MemberTableRow.tsx
@@ -0,0 +1,33 @@
+import { MemberWithMembershipFees } from '~/shared/api';
+import { TableCell, TableRow } from '~/shared/ui';
+
+import { FeeTableCell } from './FeeTableCell';
+
+type Props = {
+  member: MemberWithMembershipFees;
+  months: number[];
+};
+
+export function MemberTableRow(props: Props) {
+  const { firstName, lastName, membershipFees } = props.member;
+  const months = props.months;
+
+  const fees = new Map();
+  membershipFees.map((fee) => {
+    fees.set(fee.reportingMonth, fee.amount);
+  });
+
+  return (
+    <TableRow key={lastName}>
+      <TableCell className="w-32 py-2">
+        <span className="text-sm opacity-70">{lastName}</span>
+        <br />
+        <span>{firstName}</span>
+      </TableCell>
+      {months.map((month, index) => {
+        const paidFee = fees.get(month);
+        return <FeeTableCell month={month} index={index} paidFee={paidFee} />;
+      })}
+    </TableRow>
+  );
+}
