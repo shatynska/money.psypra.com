ef30e7225beb1256f51674a64c97fb61fd2de8f3
Helen Shatynska <helen.shatynska@gmail.com>
2023-12-01 18:40:20 +0200

    Add Suspense and ErrorBoundary to CurrentCashBook

--- src/entities/current-cash-book/ui/CurrentCashBook.tsx
+++ src/entities/current-cash-book/ui/CurrentCashBook.tsx
@@ -1,33 +1,30 @@
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+
 // eslint-disable-next-line import/no-internal-modules
 import { useCashBooksControllerGetCurrentCashBooks } from '~/shared/api/generated/hooks/cashBooksController';
+import { Skeleton } from '~/shared/ui';
 
 import { MembershipFees } from './MembershipFees';
 
 export function CurrentCashBook() {
-  const {
-    data: response,
-    isLoading,
-    error,
-  } = useCashBooksControllerGetCurrentCashBooks();
-  const currentCashBook = response;
+  const { data: currentCashBook } = useCashBooksControllerGetCurrentCashBooks();
+
+  // TODO Add fallback to ErrorBoundary
+  // TODO Add Expenses component
 
   return (
-    <>
-      {error instanceof Error && <div>{error.message}</div>}
-      {isLoading ? (
-        <span>Завантаження ...</span>
-      ) : currentCashBook ? (
-        <>
+    currentCashBook && (
+      <ErrorBoundary fallback={null}>
+        <Suspense fallback={<Skeleton />}>
           <MembershipFees
             reportingMonths={currentCashBook.reportingMonths}
             membersWithMembershipFees={
               currentCashBook.membersWithMembershipFees
             }
-          />{' '}
-        </>
-      ) : (
-        <span> Касова книга не знайдена</span>
-      )}
-    </>
+          />
+        </Suspense>
+      </ErrorBoundary>
+    )
   );
 }
