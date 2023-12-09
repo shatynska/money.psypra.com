21a4ef3590932bbb068f5f17c0764ec6b86b5f2e
Helen Shatynska <helen.shatynska@gmail.com>
2023-12-09 16:14:26 +0200

    Update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,15 +3,22 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'airbnb',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
     '@feature-sliced',
     '@feature-sliced/eslint-config/rules/import-order/experimental',
     'plugin:storybook/recommended',
     'plugin:prettier/recommended',
-    'prettier/@typescript-eslint',
+    'prettier',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'src/shared/api/generated'],
+  ignorePatterns: [
+    'build',
+    '.eslintrc.cjs',
+    'src/shared/api/generated',
+    'notes',
+  ],
   parser: '@typescript-eslint/parser',
   settings: {
     'import/resolver': {
@@ -20,12 +27,42 @@ module.exports = {
       },
     },
   },
-  plugins: ['react-refresh', 'prettier'],
+  plugins: ['react', 'react-refresh', '@typescript-eslint', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.tsx'],
+      },
+    ],
+    'react/require-default-props': [
+      'warn',
+      {
+        functions: 'defaultArguments',
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          '**/*.config*',
+          '**/*.test.*',
+          '**/*.spec.*',
+          '**/*.stories.*',
+          '**/msw/**',
+          '**/mock*/**',
+          '**/vitest*',
+        ],
+      },
+    ],
     'prettier/prettier': 'error',
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
   },
 };
